633471544d376baf280ac86a4bbc67088cdb5e1a
Hannalee Wu <hannaleehzw@gmail.com>
2024-09-15 01:22:23 -0400

    removed unnecessary things

--- htn2024-real/convex/schema.ts
+++ htn2024-real/convex/schema.ts
@@ -1,16 +1,11 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
-import { format } from "path";
 
 // The schema is entirely optional.
 // You can delete this file (schema.ts) and the
 // app will continue to work.
 // The schema provides more precise TypeScript types.
 export default defineSchema({
-  messages: defineTable({
-    author: v.string(),
-    body: v.string(),
-  }),
   camera_data: defineTable({
     storageId: v.string(),  // storageId of images of pedestrians who violate rules
     crosswalk_name: v.string(), // implement later --> to identify different crosswalks/intersections
